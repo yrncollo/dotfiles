6b510c9fe34b1c75bc9acc8b873e0dccdce45583
yrncollo <ngenocollins599@gmail.com>
2023-08-30 21:52:53 +0300

    created a setup file

---
+++ Desktop/machine_setup.py
@@ -0,0 +1,52 @@
+#!/bin/python3
+# @author:YrnCollo
+
+'''imports'''
+import os
+
+arch = "pacman"
+
+
+def os_check():
+    '''checking the os version'''
+    uname = os.popen('uname -a').read()
+    uname = uname.split()[2]
+    distro = uname.split('-')[1][:4]
+    return distro
+
+
+def install_packages():
+    '''installing packages'''
+    os.system(f'sudo {arch} -S git')
+    os.system(f'sudo {arch} -S nvim')
+    os.system(f'sudo {arch} -S xclip')
+    os.system(f'sudo {arch} -S zsh-autosuggestions')
+    os.system(f'sudo {arch} -S zsh-syntax-highlighting')
+
+
+def vim_setup():
+    '''vim setup packages'''
+    os.system(f'sudo {arch} -S python-jedi')
+    os.system(f'sudo {arch} -S flake8')
+    os.system(f'sudo {arch} -S python-jedi')
+    os.system(f'sudo {arch} -S python-pylint')
+    os.system(f'sudo {arch} -S yarn')
+    os.system('yarn add npm')
+
+
+def window_manager():
+    '''gnome setup packages'''
+    os.system(f'sudo {arch} -S xorg-xprop')
+    os.system(f'sudo {arch} -S gnome-shell-extension-manager')
+
+
+def other_apps():
+    '''this are other applications installed'''
+    os.system(f'sudo {arch} -S discord')
+    os.system(f'sudo {arch} -S chrome')
+    os.system(f'sudo {arch} -S firefox')
+    os.system('yay -S notion-app-enhanced')
+
+
+# os_check()
+# install_packages()
